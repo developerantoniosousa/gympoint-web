96b6b0439d90265755d7dc4718563f12592416da
Antonio Sousa <developerantoniosousa@gmail.com>
2020-01-04 18:42:47 -0300

    feat: add token for all requests

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -13,6 +13,8 @@ export function* signIn({ payload }) {
     const response = yield call(api.post, '/sessions', { email, password })
 
     const { token, user } = response.data;
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
     yield put(signInSuccess(token, user));
     history.push('/students');
   } catch (err) {
@@ -21,11 +23,17 @@ export function* signIn({ payload }) {
   }
 }
 
+export function setToken({ payload }) {
+  const { token } = payload.auth
+  api.defaults.headers.Authorization = `Bearer ${token}`;
+}
+
 export function signOut() {
   history.push('/');
 }
 
 export default all([
+  takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_OUT', signOut),
 ]);
