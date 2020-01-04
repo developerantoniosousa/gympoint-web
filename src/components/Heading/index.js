8480ced5247b337e091c138402f2e73b85680bc1
Antonio Sousa <developerantoniosousa@gmail.com>
2020-01-04 18:01:50 -0300

    feat: add heading component

---
+++ src/components/Heading/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  color: #444;
+`;
+
+export default Heading;
