4e1dfc4f8bc8b95d69cb1e128e9ee934f3e6dcaa
Antonio Sousa <developerantoniosousa@gmail.com>
2020-01-04 22:49:35 -0300

    feat: add column component

---
+++ src/components/Column/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Row = styled.div`
+  width: 100%;
+  padding: 0 7.5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export default Row;
