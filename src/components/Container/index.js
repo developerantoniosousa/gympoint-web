c625b1d65fe7db85a608f7030d70d3505bacbb06
Antonio Sousa <developerantoniosousa@gmail.com>
2020-01-04 23:45:17 -0300

    feat: add size prop on container component

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,10 +1,31 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+function setSize(size) {
+  switch (size) {
+    case 'small': {
+      return css`
+        width: 700px;
+      `;
+    }
+    case 'medium': {
+      return css`
+        width: 900px;
+      `;
+    }
+    default: {
+      return css`
+        width: 100%;
+      `;
+    }
+  }
+}
 
 const Container = styled.div`
   width:100%;
   max-width: 1200px;
   margin: 0 auto;
   padding: 30px 0;
+  ${({ size }) => setSize(size)};
 `;
 
 export default Container;
